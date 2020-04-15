b5b796ad39f199b6962c400a5f5586ff8c00130f
Richard Zvonek <zvonek.r@seznam.cz>
2020-04-15 22:47:05 +0200

    Unions usages examples

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# unions-training
+# Internal Unions training
+## 1. What the hell is union? 
+A union is a special class type that can hold only one of its non-static data members at a time. The class specifier for a union declaration is similar to class or struct declaration. 
+
+```c++
+union myUnion{
+  int someMember;
+  float anotherMember;
+}
+```
+
+Union may have member functions, but cannot have virtual functions.
+
+Union is only as big as necessary to hold its largest data member. The other data members are allocated in the same bytes as part of that largest member. The details of that allocation are implementation-defined but all non-static data members will have the same address.
+
+It's **undefined behavior** to read from the member of the union that wasn't most recently written. Many compilers implement, as a non-standard language extension, the ability to read inactive members of a union. 
+
+## 2. Use cases
+### 2.1 Saving memory
+A union allows a single piece of memory to be used for different types of objects at different times. Consequently, it can be used to save memory when we have several objects that are never used at the same time.
+
+> It is like a room in a hotel. Different people live in it for non-overlapping periods of time. These people never meet, and generally don't know anything about each other. By properly managing the time-sharing of the rooms (i.e. by making sure different people don't get assigned to one room at the same time), a relatively small hotel can provide accommodations to a relatively large number of people, which is what hotels are for. 
+>
+> -Somebody on StackOverflow 
+```c++
+union value {
+  int x;
+  double d;
+};
+
+value v = { 123 };              // now v holds an int
+std::cout << v.x << std::endl;  // write 123
+v.d = 987.654;                  // now v holds a double
+std::cout << v.d << std::endl;  // write 987.654
+```
+What is wrong here? 
+
+Unions shouldn't be used 'naked'. In this use case, it is useful and safer to have some indicator of currently used member. 
+
--- src/example01.cpp
+++ src/example01.cpp
@@ -1,9 +1,163 @@
 #include <iostream>
 
-union vec3f{
-
+union NakedUnion {
+  int itgr;
+  double dbl;
 };
 
-int main(int argc, char** argv){
+struct TaggedUnion {
+  enum DataType {
+    INTEGER, DOUBLE
+  };
+  
+  DataType dataType;
+  
+  union {
+    int itgr;
+    double dbl;
+  };
+  
+  void print() {
+    print(dataType);
+  }
+  
+  void print(DataType type) {
+    switch (type) {
+      case INTEGER: {
+        std::cout << itgr;
+        break;
+      }
+      case DOUBLE: {
+        std::cout << dbl;
+        break;
+      }
+    };
+  }
+};
 
+struct RobustTaggedUnion {
+  enum DataType {
+    INTEGER, DOUBLE
+  };
+private:
+  DataType dataType;
+  
+  union {
+    int itgr;
+    double dbl;
+  };
+public:
+  void print() {
+    print(dataType);
+  }
+  
+  void print(DataType type) {
+    switch (type) {
+      case INTEGER: {
+        std::cout << itgr;
+        break;
+      }
+      case DOUBLE: {
+        std::cout << dbl;
+        break;
+      }
+    };
+  }
+  
+  void set(DataType type) {
+    dataType = type;
+  }
+  
+  void set(int i) {
+    if (dataType == INTEGER) {
+      itgr = i;
+    } else {
+      throw std::runtime_error("Wrong datatype set to union");
+    }
+  }
+  
+  void set(double d) {
+    if (dataType == DOUBLE) {
+      dbl = d;
+    } else {
+      throw std::runtime_error("Wrong datatype set to union");
+    }
+  }
+  
+  template<class T>
+  T get() {
+    throw std::runtime_error("Invalid data type");
+  }
+};
+
+template<>
+double RobustTaggedUnion::get() {
+  if (dataType == DOUBLE) {
+    return dbl;
+  }
+  throw std::runtime_error("You tried to get invalid datatype");
+}
+
+template<>
+int RobustTaggedUnion::get() {
+  if (dataType == INTEGER) {
+    return itgr;
+  }
+  throw std::runtime_error("You tried to get invalid datatype");
+}
+
+template<>
+RobustTaggedUnion::DataType RobustTaggedUnion::get() {
+  return dataType;
+}
+
+int main(int argc, char **argv) {
+  // Different possibilities, how to initialize union value
+  NakedUnion nu1{10};
+  NakedUnion nu2{.dbl = 20.};
+  NakedUnion nu3{itgr : 30}; //deprecated
+  
+  std::cout << "nu1 " << nu1.dbl << "\n";
+  std::cout << "nu2 " << nu2.dbl << "\n";
+  std::cout << "nu3 " << nu3.itgr << "\n";
+  
+  std::cout << "Why is the first output incorrect? \n\n\n";
+  
+  
+  TaggedUnion tu1{TaggedUnion::DataType::INTEGER, 10};
+  TaggedUnion tu2{.dataType{TaggedUnion::DataType::DOUBLE}, .dbl{20.}};
+  TaggedUnion tu3{dataType : TaggedUnion::DataType::INTEGER, dbl:30};
+  
+  std::cout << "tu1 ";
+  tu1.print();
+  std::cout << "\n";
+  
+  std::cout << "tu2 ";
+  tu2.print();
+  std::cout << "\n";
+  
+  std::cout << "tu3 ";
+  tu3.print();
+  std::cout << "\n";
+  
+  std::cout << "Why is the third output incorrect? \n\n\n";
+  
+  RobustTaggedUnion rtu;
+  rtu.set(RobustTaggedUnion::DOUBLE);
+  rtu.set(10.);
+  
+  try {
+    std::cout << rtu.get<int>();
+  }
+  catch (std::runtime_error ex) {
+    std::cout << ex.what() << "\n";
+  }
+  
+  try {
+    std::cout << rtu.get<double>();
+  }
+  catch (std::runtime_error ex) {
+    std::cout << ex.what() << "\n";
+  }
+  
 }
